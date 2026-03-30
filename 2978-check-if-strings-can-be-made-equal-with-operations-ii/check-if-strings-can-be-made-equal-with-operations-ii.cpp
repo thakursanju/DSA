1c783c7b11b4f3a9e41337ff7f70df00134d0b06
Khushvinder Thakur <sanjuthakur383838@gmail.com>
2026-03-31 04:08:55 +0530

    Time: 4 ms (84.56%) | Memory: 20 MB (48.52%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -3,10 +3,8 @@ public:
     bool checkStrings(string s1, string s2) {
         int n = s1.size();
         if(n != s2.size()) return false;
-
         vector<int> even1(26,0), odd1(26,0);
         vector<int> even2(26,0), odd2(26,0);
-
         for(int i = 0; i < n; i++){
             if(i % 2 == 0){
                 even1[s1[i] - 'a']++;
@@ -16,7 +14,6 @@ public:
                 odd2[s2[i] - 'a']++;
             }
         }
-
         return (even1 == even2 && odd1 == odd2);
     }
 };
