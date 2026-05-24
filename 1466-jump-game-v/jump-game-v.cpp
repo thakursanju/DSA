3322f2c3d52d91597d9ce53560b9c6b1cf93c139
Khushvinder Thakur <sanjuthakur383838@gmail.com>
2026-05-24 15:06:38 +0530

    Time: 7 ms (94.19%) | Memory: 19 MB (76.45%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -6,14 +6,14 @@ int solve(vector<int>& arr,vector<int> & dp,int d,int i){
     }
     int count=1;
     int n=arr.size();
-    for(int j=i+1;j<=min(n-1,i+d);j++){
-        if(arr[j]>=arr[i]) break;
-        count=max(count,1+solve(arr,dp,d,j));
-    }    
     for(int j=i-1;j>=max(0,i-d);j--){
         if(arr[j]>=arr[i]) break;
         count=max(count,1+solve(arr,dp,d,j));
     }
+    for(int j=i+1;j<=min(n-1,i+d);j++){
+        if(arr[j]>=arr[i]) break;
+        count=max(count,1+solve(arr,dp,d,j));
+    }    
     return dp[i]=count;
 
 }
@@ -22,7 +22,7 @@ public:
         int n=arr.size();
         vector<int> dp(n+1,-1);
         int ans=0;
-        for(int i=0;i<n;i++){
+        for(int i=n-1;i>=0;i--){
          ans = max(ans,solve(arr,dp,d,i));
         }
         return ans;
