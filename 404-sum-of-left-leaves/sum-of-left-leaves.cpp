01dbfa69298e1e6628d155e8c2992575aa0724f4
Khushvinder Thakur <sanjuthakur383838@gmail.com>
2026-06-27 02:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (56.74%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root) return 0;
+
+        int ans = 0;
+
+        if (root->left &&
+            !root->left->left &&
+            !root->left->right) {
+            ans += root->left->val;
+        }
+
+        ans += sumOfLeftLeaves(root->left);
+        ans += sumOfLeftLeaves(root->right);
+
+        return ans;
+    }
+};
