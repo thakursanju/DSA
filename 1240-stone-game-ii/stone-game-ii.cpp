4f04fa6eff3a8b2ffe9c59864bc309a5d98e507e
Khushvinder Thakur <sanjuthakur383838@gmail.com>
2026-08-09 17:57:02 +0530

    Time: 3 ms (95.69%) | Memory: 13.6 MB (55.90%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,32 +1,27 @@
 class Solution {
 private:
-   int solve(int i,int M,vector<int>& piles,vector<vector<int>>& dp,int n,vector<int>& sufix){
-    if(i>=n) return 0;
+   int solve(int i,int M,vector<int> & piles,vector<vector<int>> &dp,vector<int> & suffix){
+    if(i>=piles.size()) return 0;
     if(dp[i][M]!=-1) return dp[i][M];
-    // fisrt alice m=1 always 
-    if(i+2*M>=n){
-        return sufix[i];
+    if(i+2*M>=piles.size()){
+        return suffix[i];
     }
     int ans=0;
-    for(int k=1;k<=2*M;k++){
-        int take=solve(i+k,max(k,M),piles,dp,n,sufix);
-        ans=max(ans,sufix[i]-take);
+    for(int i1=1;i1<=2*M;i1++){
+        int take=solve(i+i1,max(i1,M),piles,dp,suffix);
+        ans=max(ans,suffix[i]-take);
     }
-
-return dp[i][M]=ans;
-    }
-   
+    return dp[i][M]=ans;
+   }
 public:
     int stoneGameII(vector<int>& piles) {
         int n=piles.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        vector<int> sufix(n+1,0);
-        sufix[n-1] = piles[n-1];
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));// maxi /
+        vector<int> suffix(n+1,0);
+        suffix[n-1]=piles[n-1];
         for(int i=n-2;i>=0;i--){
-            sufix[i]=sufix[i+1]+piles[i];
+            suffix[i]=suffix[i+1]+piles[i];
         }
-        return solve(0,1,piles,dp,n,sufix);
-
-        
+        return solve(0,1,piles,dp,suffix);
     }
 };
