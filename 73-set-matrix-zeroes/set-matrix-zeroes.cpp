9afb86597ced57df3dd6a6170e498857f03c0804
Khushvinder Thakur <sanjuthakur383838@gmail.com>
2026-08-28 11:17:41 +0530

    Time: 43 ms (5.30%) | Memory: 21.9 MB (5.90%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<pair<int,int>> al;
+        int n =matrix.size();
+        int m =matrix[0].size();
+        vector<int> r;
+        vector<int> c;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    al.push_back({i,j});
+                    c.push_back(j);
+                    r.push_back(i);
                 }
             }
         }
-        for(auto i : al){
+        for(int i=0;i<r.size();i++){
             for(int j=0;j<m;j++){
-                matrix[i.first][j]=0;
+                matrix[r[i]][j]=0;
             }
         }
-        for(auto i : al){
+        for(int i=0;i<c.size();i++){
             for(int j=0;j<n;j++){
-                matrix[j][i.second]=0;
+                matrix[j][c[i]]=0;
             }
-        }  
+        }
+        
     }
 };
