701a363659c91eaf0fa0f8c5807274822cf032bf
Khushvinder Thakur <sanjuthakur383838@gmail.com>
2026-09-05 01:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (36.78%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> pre(n,0);
+        pre[0]=nums[0];
+
+        vector<int> suf(n,0);
+        suf[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            pre[i]=max(pre[i-1],nums[i]);
+        }
+        for(int i=n-2;i>=0;i--){
+            suf[i]=min(suf[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            int x=abs(pre[i]-suf[i]);
+            if(x<=k){
+                return i;
+            }
+        }
+            return -1;
+    }
+};
